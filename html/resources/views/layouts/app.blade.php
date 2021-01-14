cc8d9ed4edf0e287a1790a9c472214bd9f7c9d2f
Florian P <palabostf@gmail.com>
2021-01-14 11:13:27 +0100

    [setup] starter project laravel

---
+++ html/resources/views/layouts/app.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Laraflo - @yield('title')</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('css')
+</head>
+<body>
+    <div id="app" class="d-flex wrapper">
+
+        @auth()
+            @include('layouts.navbars.sidebar')
+        @endauth
+
+        @if (session('status'))
+            <div class="card-body">
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+            </div>
+        @endif
+        <div id="page-content-wrapper">
+            @include('layouts.navbars.nav')
+            <div class="container-fluid">
+                @yield('content')
+            </div>
+
+            @include('layouts.footers.footer')
+        </div>
+    </div>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    @stack('script')
+</body>
+</html>
---
+++ html/resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+@section('title')
+    Accueil
+@endsection
+@section('content')
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-12">
+                <p class="w-100 text-justify">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed dictum justo, ut suscipit ligula. Morbi ultricies magna libero, eget laoreet nisl blandit feugiat. Aenean quis interdum elit. Cras elementum enim sed quam blandit, eget vehicula sapien dictum. Nam placerat, tortor quis elementum sagittis, nulla risus finibus nibh, eu congue ex ipsum id leo. Mauris id fringilla tortor, a tincidunt purus. Proin tortor enim, vehicula vitae gravida sit amet, aliquam sed dui. Nunc at erat hendrerit, pharetra arcu non, hendrerit velit. Mauris vitae lectus quis erat semper lacinia nec eu enim. Vestibulum tempor vitae est commodo faucibus. Nam fringilla metus nec lectus vehicula tristique. Vivamus porta justo et dui pulvinar, non malesuada leo elementum. Suspendisse vehicula eget sem in faucibus. Nam id pulvinar augue, at rhoncus est.
+
+                    Quisque in est ac dui suscipit condimentum id at augue. Cras mattis accumsan metus in consequat. Quisque ac varius mi. Maecenas eleifend tempus elit, sed iaculis dolor. Etiam accumsan non tortor lobortis malesuada. Duis lacus quam, hendrerit ac mauris et, consequat facilisis est. Vivamus blandit massa quis vulputate varius.
+
+                    Nunc vitae scelerisque ligula. Fusce scelerisque sapien non ultricies lacinia. Quisque elementum mattis nulla, id tincidunt dolor ullamcorper ac. Mauris nisl nisi, varius sed suscipit sed, hendrerit sed augue. Praesent suscipit, arcu sed laoreet pretium, est enim iaculis eros, sit amet malesuada mauris leo posuere massa. Phasellus sed suscipit justo. Fusce ligula nisl, ultricies at lectus id, tincidunt luctus velit. In hac habitasse platea dictumst. Pellentesque aliquam diam leo, vel posuere tortor congue facilisis. Fusce molestie pulvinar tellus, a varius tortor dignissim quis. Nullam eu vulputate velit.
+                </p>
+            </div>
+        </div>
+    </div>
+@endsection
